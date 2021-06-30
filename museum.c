0f72ba351890353baf109111cd508a1081ccd3c9
sBujowski <s.bujowski@gmail.com>
2021-07-01 00:59:03 +0200

    everything seems good

--- museum.c
+++ museum.c
@@ -41,12 +41,12 @@ void createNo(const int Na, const int Nb) {
         }
         sem_t *hallA = sem_open("/hallA", O_CREAT, 0600, Na);
         if (hallA == SEM_FAILED) {
-            fprintf(stdout, "sem_open /workA\n");
+            fprintf(stdout, "sem_open /hallA\n");
             exit(10);
         }
         sem_t *hallB = sem_open("/hallB", O_CREAT, 0600, Nb);
         if (hallB == SEM_FAILED) {
-            fprintf(stdout, "sem_open /workA\n");
+            fprintf(stdout, "sem_open /hallB\n");
             exit(10);
         }
         fprintf(stdout, "Museum opened (no. of visitors)\n");
@@ -88,7 +88,7 @@ void createTime(const int Na, const int Nb) {
             seconds += 2;
             sem_getvalue(hallA, &valA);
             sem_getvalue(hallB, &valB);
-            fprintf(stdout, "\tvisitors:\thallA = %d/%d,\thallB = %d/%d\ttime: %lus\n", Na - valA, Na, Nb - valB, Nb, seconds);
+            fprintf(stdout, "\tvisitors:\thallA = %d/%d,\thallB = %d/%d\t(A+reserved = %d/%d)\ttime: %lus\n", Na - valA - (Nb - valB), Na, Nb - valB, Nb, Na - valA, Na, seconds);
         }
     } else
         fprintf(stdout, "Na must be greater than Nb\n");
@@ -144,12 +144,15 @@ void museumAddA(const int x) {
         while (1) {
             fprintf(stdout, "[V#%d] waiting A\n", pid);
             sem_wait(hallA);
+
             time = rand() % 10 + 2;
             fprintf(stdout, "[V#%d] entered A, watching for %ds\n", pid, time);
             sleep(time);
+
             fprintf(stdout, "[V#%d] leaving A\n", pid);
             sleep(1);
             sem_post(hallA);
+
             fprintf(stdout, "[V#%d] left A\n", pid);
             sleep(rand() % 10 + 2);
         }
@@ -167,14 +170,17 @@ void museumAddA(const int x) {
         while (1) {
             fprintf(stdout, "[V#%d] waiting A\n", pid);
             sem_wait(workA);
-            sem_wait(hallA); // only to decrease count in hallA
+            sem_wait(hallA);
+
             time = rand() % 10 + 2;
             fprintf(stdout, "[V#%d] entered A, watching for %ds\n", pid, time);
             sleep(time);
+
             fprintf(stdout, "[V#%d] leaving A\n", pid);
             sleep(1);
-            sem_post(workA);
             sem_post(hallA);
+            sem_post(workA);
+
             fprintf(stdout, "[V#%d] left A\n", pid);
             sleep(rand() % 10 + 2);
         }
@@ -182,5 +188,120 @@ void museumAddA(const int x) {
 }
 
 void museumAddB(const int x) {
+    int f = 0;
+    switch (x) {
+        case 1:
+            break;
+        case 2:
+            f++;
+            break;
+        case 4:
+            f += 2;
+            break;
+        case 8:
+            f += 3;
+            break;
+        default:
+            fprintf(stdout, "Wrong number of visitors\n\n");
+            return;
+    }
 
+    sem_t *hallA = sem_open("/hallA", O_EXCL);
+    if (hallA == SEM_FAILED) {
+        fprintf(stdout, "Museum not opened (hallA not found)\n");
+        return;
+    }
+    sem_t *hallB = sem_open("/hallB", O_EXCL);
+    if (hallB == SEM_FAILED) {
+        fprintf(stdout, "Museum not opened (hallB not found)\n");
+        return;
+    }
+
+    fprintf(stdout, "(press ctrl+D to start)\n\n");
+    char c;
+    scanf("%c", &c);
+
+    // Check what version of the museum is opened
+    sem_t *workA = sem_open("/workA", O_EXCL);
+    if (workA == SEM_FAILED) {
+        // time of leaving B
+        while (f > 0) {
+            fork();
+            f--;
+        }
+        pid_t pid = getpid();
+        srand(pid);
+
+        int time;
+        time_t start, stop;
+        sleep(rand() % 5);
+        while (1) {
+            fprintf(stdout, "[V#%d] waiting A\n", pid);
+            sem_wait(hallA);
+
+            time = rand() % 10 + 2;
+            fprintf(stdout, "[V#%d] entered A, watching for %ds\n", pid, time);
+            sleep(time);
+
+            fprintf(stdout, "[V#%d] waiting B\n", pid);
+            sem_wait(hallB);
+
+            time = rand() % 10 + 2;
+            fprintf(stdout, "[V#%d] entered B, watching for %ds\n", pid, time);
+            sleep(time);
+
+            fprintf(stdout, "[V#%d] leaving B and A\n", pid);
+            start = clock();
+            sleep(1);
+            sem_post(hallB);
+            sleep(1);
+            sem_post(hallA);
+
+            stop = clock();
+            fprintf(stdout, "[V#%d] left B and A in %.5f\n", pid, (float) (stop - start) / CLOCKS_PER_SEC + 2); // +2 to account for `sleep`
+            sleep(rand() % 10 + 2);
+        }
+    } else {
+        // no. of visitors
+        while (f > 0) {
+            fork();
+            f--;
+        }
+        pid_t pid = getpid();
+        srand(pid);
+
+        int time;
+        sleep(rand() % 5);
+        while (1) {
+            fprintf(stdout, "[V#%d] waiting A\n", pid);
+            sem_wait(workA);
+            sem_wait(hallA);
+
+            time = rand() % 10 + 2;
+            fprintf(stdout, "[V#%d] entered A, watching for %ds\n", pid, time);
+            sleep(time);
+
+            fprintf(stdout, "[V#%d] waiting B\n", pid);
+            sem_wait(hallB);
+
+            time = rand() % 10 + 2;
+            fprintf(stdout, "[V#%d] entered B, watching for %ds\n", pid, time);
+            sem_post(workA);
+            sem_post(hallA);
+            //sem_post(workA);
+            sleep(time);
+
+            fprintf(stdout, "[V#%d] leaving B\n", pid);
+            sleep(1);
+            sem_wait(hallA);
+
+            sem_post(hallB);
+            fprintf(stdout, "[V#%d] leaving A (from B)\n", pid);
+            sleep(1);
+            sem_post(hallA);
+
+            fprintf(stdout, "[V#%d] left A\n", pid);
+            sleep(rand() % 10 + 2);
+        }
+    }
 }
